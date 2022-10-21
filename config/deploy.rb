0b63461ae28168cb13ef27ddb1c5294589209d56
Douglas A. Seifert <doug@uptake.com>
2022-10-21 13:15:11 -0700

    add passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,21 @@ lock "~> 3.17.1"
 
 set :application, "bbo-free"
 set :repo_url, "git@github.com:seifertd/bbo-free.git"
+set :user, "doug"
+set :group, "doug"
+set :use_sudo, false
+set :tmp_dir, "/tmp"
 
 # Default branch is :master
-set :branch, 'main'
+set :branch, ENV['BRANCH'] || 'main'
 
 # rvm
 set :rvm_ruby_version, "ruby-3.1.1"
+set :rvm_type, :system
 
 set :log_level, :debug
+
+set :passenger_restart_with_touch, true
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
@@ -26,10 +33,10 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, 'config/database.yml', 'config/master.key', 'config/secrets.yml', 'db/production.sqlite3'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 set :default_env, { rvm_bin_path: "/usr/local/rvm/bin" }
@@ -38,7 +45,7 @@ set :default_env, { rvm_bin_path: "/usr/local/rvm/bin" }
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
