bdb181cbd30dce0583292d1082f735f002bca69a
Douglas A. Seifert <doug@uptake.com>
2022-10-30 11:49:33 -0700

    Get the turbo streams implementation working
    * call requestSubmit() on the form
    * handle tournament updates
    * update flash content correctly

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -9,15 +9,19 @@ class TournamentsController < ApplicationController
 
   def create
     parser = BboParser.new(params[:tournament_results_html])
-    @tournament, entry, new_tourney = parser.parse
+    @tournament, entry, @new_tourney = parser.parse
+    if @new_tourney
+      flash.now[:success] = "Entry #{entry.player} was recorded!"
+    else
+      flash.now[:info] = "Entry #{entry.player} was updated!"
+    end
     respond_to do |format|
-      puts "FORMAT: #{format.inspect}"
       format.turbo_stream
       format.html do
         if new_tourney
-          redirect_to action: 'index', success: "Entry #{entry.player} was recorded!"
+          redirect_to action: 'index'
         else
-          redirect_to action: 'index', info: "Entry #{entry.player} was updated!"
+          redirect_to action: 'index'
         end
       end
     end
