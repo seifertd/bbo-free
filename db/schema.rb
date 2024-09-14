677570f2bcd806ae5130e4ac64b5febf0c55e06a
Douglas A. Seifert <doug@dseifert.net>
2024-09-14 07:08:43 -0700

    rails 7.1 upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_234735) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_14_140219) do
   create_table "boards", force: :cascade do |t|
     t.integer "entry_id", null: false
     t.integer "number"
